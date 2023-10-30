3ef47c31a6d4fc83be489de161c14c123c00b713
GamaRayL <gamaizingg@gmail.com>
2023-10-30 10:04:52 +0300

    Внесены поправки в PaymentListAPIView

--- lms/views/payment.py
+++ lms/views/payment.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, generics
+from rest_framework.filters import OrderingFilter
 from lms.serializers.payment import PaymentSerializer
 
 from lms.models import Payment
@@ -8,9 +9,9 @@ from lms.models import Payment
 class PaymentListAPIView(generics.ListAPIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-    # filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    # filterset_class = PaymentFilter
-    # ordering_fields = ['payment_date']
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('payment_method', 'paid_course_or_lesson')
+    ordering_filter = ('payment_date')
 
     def get_queryset(self):
         queryset = Payment.objects.all()
