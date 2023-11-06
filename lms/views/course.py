e8ab8bf9e6db204c21e5d53a8e42829eaa4bc409
Gamid <gamaizingg@gmail.com>
2023-11-06 12:18:01 +0300

    Обновлены методы CourseViewSet и оставлен один мертвый код внутри класса

--- lms/views/course.py
+++ lms/views/course.py
@@ -14,24 +14,35 @@ class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     permission_classes = [IsAuthenticated, IsModerator | IsOwner]
 
-    def get_object(self):
-        course = super().get_object()
-        if course.owner != self.request.user:
-            raise PermissionDenied({'detail': 'У вас нет разрешения на доступ к этому уроку.'})
-        return course
+    # def get_object(self):
+    #     course = super().get_object()
+    #     if course.owner != self.request.user:
+    #         raise PermissionDenied({'detail': 'У вас нет разрешения на доступ к этому уроку.'})
+    #     return course
+    def list(self, request, *args, **kwargs):
+        if IsModerator().has_permission(self.request, self):
+            queryset = Course.objects.all()
+        else:
+            queryset = Course.objects.filter(owner=self.request.user)
 
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        if instance.owner != request.user:
-            raise PermissionDenied({'detail': 'У вас нет разрешения на доступ к этому уроку.'})
-        serializer = self.get_serializer(instance)
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
-    def get_queryset(self):
-        if IsModerator().has_permission(self.request, self):
-            return Course.objects.all()
-        else:
-            return Course.objects.filter(owner=self.request.user)
+    def retrieve(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.owner != request.user:
+            raise PermissionDenied({'detail': 'У вас нет разрешения на доступ к этому уроку.'})
+        serializer = self.get_serializer(obj)
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.owner != self.request.user:
+            return Response({'error': 'У вас нет разрешения на обновление этого урока.'}, status=403)
+        serializer = self.get_serializer(obj, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
         if IsModerator().has_permission(request, self):
@@ -40,4 +51,3 @@ class CourseViewSet(viewsets.ModelViewSet):
     def destroy(self, request, *args, **kwargs):
         if IsModerator().has_permission(request, self):
             return Response({'error': 'У вас нет разрешения на удаление уроков.'}, status=403)
-
--- lms/views/lesson.py
+++ lms/views/lesson.py
@@ -1,5 +1,4 @@
-from rest_framework import generics, status
-from rest_framework.exceptions import PermissionDenied
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
